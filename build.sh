ada46bd6f4dda52b7baace1ed3c5a0ddfa7e18c3
Kevin Barabash <kevinb7@gmail.com>
2014-12-02 20:07:33 -0700

    include node friendly modules

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-mkdir -p temp &&
-./node_modules/.bin/tsc src/eventsim.ts --outDir temp --target ES5 --declaration --module commonjs --removeComments &&
+./node_modules/.bin/tsc src/eventsim.ts --outDir lib --target ES5 --declaration --module commonjs --removeComments &&
 mkdir -p dist &&
-./node_modules/.bin/browserify temp/eventsim.js --standalone EventSim --outfile dist/eventsim.js &&
-cp temp/eventsim.d.ts dist/eventsim.d.ts
+./node_modules/.bin/browserify lib/eventsim.js --standalone EventSim --outfile dist/eventsim.js &&
+cp lib/eventsim.d.ts dist/eventsim.d.ts
 #cat src/DOMEventsLevel3.shim.min.js >> dist/eventsim.js
---
+++ lib/createKeyboardEvent.d.ts
@@ -0,0 +1,2 @@
+declare function createKeyboardEvent(type: string, dict: any): KeyboardEvent;
+export = createKeyboardEvent;
